069f5804669e7a71a33b2dac65c83cc14cd255a3
Rob Royson <rob@citibot.io>
2023-02-05 15:05:48 -0500

    Fix header spacing

--- components/Header.js
+++ components/Header.js
@@ -8,21 +8,23 @@ const Header = () => (
         <Image alt='' width={35} height={35} src='/favicon.ico'></Image>
       </div>
 
-      <Link href='#home'>
-        <button className='px-2 text-2xl tracking-tight hover:text-gray-500'>
-          Home
-        </button>
-      </Link>
-      <Link href='#technologies'>
-        <button className='px-2 text-2xl tracking-tight hover:text-gray-500'>
-          Technologies
-        </button>
-      </Link>
-      <Link href='#projects'>
-        <button className='px-2 text-2xl tracking-tight hover:text-gray-500'>
-          Projects
-        </button>
-      </Link>
+      <div className='w-3/4 text-right min-w-fit'>
+        <Link className='px-6' href='#home'>
+          <button className='px-2 text-2xl tracking-tight hover:text-gray-500'>
+            Home
+          </button>
+        </Link>
+        <Link className='px-6' href='#technologies'>
+          <button className='px-2 text-2xl tracking-tight hover:text-gray-500'>
+            Technologies
+          </button>
+        </Link>
+        <Link className='px-6' href='#projects'>
+          <button className='px-2 text-2xl tracking-tight hover:text-gray-500'>
+            Projects
+          </button>
+        </Link>
+      </div>
     </nav>
   </header>
 )
